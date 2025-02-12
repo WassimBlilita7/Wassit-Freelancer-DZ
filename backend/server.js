6c0a3b4a63c4db84eaefcf4f48296deb2bc84568
wassimblilita7 <wassim.blilita19@gmail.com>
2025-02-12 14:41:19 +0100

    installation backend + configuration du MONGODB

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+
+const app = express();
+
+
+const PORT = ENV_VARS.PORT;
+
+app.use("/api/v1/auth",authRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Le serveur est lancé sur le port ${PORT}`);
+    connectDB();
+    });
